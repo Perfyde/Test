6eadb7ff6338d7c5354a9898549cb915b930cfc2
Paul Pringault <paul.pringault@gmail.com>
2016-02-27 21:14:33 +0100

    new pic modif

--- Test.py
+++ Test.py
@@ -1,8 +1,19 @@
 from PIL import Image, ImageFilter
 
-im = Image.open("WPP.jpg")
+im = Image.open("poisson.jpg")
 
-im1 = im.filter(ImageFilter.GaussianBlur(8))
+(width, height) = im.size
+for x in range(width):
+    for y in range(height):
+        print "Processing pixel (%d %d)" % (x, y)
+        (r, g, b) = im.getpixel((x, y))
+        if 150 <= r <= 255 and 0 <= g <= 40 and 0 <= b <= 70:
+            im.putpixel((x, y), (255, 0, 0))
+        else:
+            im.putpixel((x, y), (255, 255, 255))
+
+im = im.filter(ImageFilter.GaussianBlur(8))
 
 # write to stdout
-im1.save("WPP5.jpg")
+im.save("poisson1.jpg")
+
