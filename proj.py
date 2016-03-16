c999e257504108499c35c4707cbb708beb0362ed
Paul Pringault <paul.pringault@gmail.com>
2016-03-16 09:53:34 +0100

    test new image

--- proj.py
+++ proj.py
@@ -1,21 +1,33 @@
 from PIL import Image, ImageFilter, ImageDraw
 
-im = Image.open("ban.jpg")
-draw = ImageDraw.Draw(im)
+im1 = Image.open("ban.jpg")
+draw = ImageDraw.Draw(im1)
 
-(width, height) = im.size
+
+n = 0
+
+(width, height) = im1.size
 for x in range(width):
     for y in range(height):
-        print "Processing pixel (%d %d)" % (x, y)
-        (r, g, b) = im.getpixel((x, y))
-        if 150 <= r <= 255 and 0 <= g <= 40 and 0 <= b <= 70:
-            draw.ellipse([(x, y), (x+10, y+10)], (255, 0, 0))
-        else:
-            draw.point((x, y), fill=(255, 255, 255))
+        draw.point((x, y), (255, 255, 255))
+im1.save("ban2.jpg")
+
+im2 = Image.open("ban2.jpg")
+draw.ImageDraw.Draw(im2)
+
+# for x in range(width):
+     # for y in range(height):
+        # print "Processing pixel (%d %d)" % (x, y)
+        # (r, g, b) = im.getpixel((x, y))
+        # if 150 <= r <= 255 and 0 <= g <= 40 and 0 <= b <= 70:
+            # draw.point((x, y), (255, 0, 0))
+        # else:
+            # draw.point((x, y), (255, 255, 255))
+
+# print n
+# draw.ellipse([(120, 120), (130, 130)], (255, 0, 0))
+
+# im = im.filter(ImageFilter.GaussianBlur(3))
+# draw.ellipse([(150, 150), (160, 160)], (255, 0, 0))
 
 
-
-#im = im.filter(ImageFilter.GaussianBlur(3))
-#draw.ellipse([(150, 150), (160, 160)], (255, 0, 0))
-
-im.save("ban1.jpg")
