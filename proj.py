92a88fb1ec7c503bf0f6c499fc0ec04cb0397bf6
Paul Pringault <paul.pringault@gmail.com>
2016-03-16 14:25:44 +0100

    New image with ellipses

--- proj.py
+++ proj.py
@@ -7,22 +7,21 @@ draw = ImageDraw.Draw(im1)
 n = 0
 
 (width, height) = im1.size
+im2 = Image.new("RGB", (width, height), (255, 255, 255))
+im2.save("Bant.jpg")
+
+d = ImageDraw.Draw(im2)
+
 for x in range(width):
     for y in range(height):
-        draw.point((x, y), (255, 255, 255))
-im1.save("ban2.jpg")
+        print "Processing pixel im2 (%d %d)" % (x, y)
+        (r, g, b) = im1.getpixel((x, y))
+        if 150 <= r <= 255 and 0 <= g <= 40 and 0 <= b <= 70:
+            d.ellipse([(x, y), (x+10, y+10)], (255, 0, 0))
+im2.save("ban2.jpg")
 
-im2 = Image.open("ban2.jpg")
-draw.ImageDraw.Draw(im2)
 
-# for x in range(width):
-     # for y in range(height):
-        # print "Processing pixel (%d %d)" % (x, y)
-        # (r, g, b) = im.getpixel((x, y))
-        # if 150 <= r <= 255 and 0 <= g <= 40 and 0 <= b <= 70:
-            # draw.point((x, y), (255, 0, 0))
-        # else:
-            # draw.point((x, y), (255, 255, 255))
+
 
 # print n
 # draw.ellipse([(120, 120), (130, 130)], (255, 0, 0))
